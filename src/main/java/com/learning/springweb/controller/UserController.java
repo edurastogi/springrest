4ed681a85cf9205ceb8dd2b015a05796639c3a00
aditya rastogi <edurastogi@gmail.com>
2021-05-28 23:38:54 -0400

    added exception handling code

--- src/main/java/com/learning/springweb/controller/UserController.java
+++ src/main/java/com/learning/springweb/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -34,7 +36,7 @@ public class UserController{
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
         User savedUser = service.save(user);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -42,6 +44,14 @@ public class UserController{
                 .buildAndExpand(savedUser.getId())
                 .toUri();
         return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user == null){
+            throw new UserNotFoundException("id-" + id);
+        }
     }
 
 }
--- src/main/java/com/learning/springweb/model/User.java
+++ src/main/java/com/learning/springweb/model/User.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Getter
@@ -13,6 +15,10 @@ import java.util.Date;
 @ToString
 public class User{
     private Integer id;
+
+    @Size(min=2, message = "Name should have at least 2 characters.")
     private String name;
+
+    @Past
     private Date birthDate;
 }
